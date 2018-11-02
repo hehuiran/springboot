bb6060e0be9d941cccf3881cd628daec21ae77f9
hehuiran <15260828327@163.com>
2018-11-03 00:43:47 +0800

    fix:update closeUtils

--- src/main/java/com/yz/springboot/util/CloseUtils.java
+++ src/main/java/com/yz/springboot/util/CloseUtils.java
@@ -4,7 +4,7 @@ import java.io.Closeable;
 import java.io.IOException;
 
 /**
- * author: huiRan
+ * @author huiRan
  * date: 2018/11/1 20:12
  */
 public final class CloseUtils {
@@ -19,7 +19,9 @@ public final class CloseUtils {
      * @param closeables closeables
      */
     public static void closeIO(final Closeable... closeables) {
-        if (closeables == null) return;
+        if (closeables == null) {
+            return;
+        }
         for (Closeable closeable : closeables) {
             if (closeable != null) {
                 try {
@@ -37,7 +39,9 @@ public final class CloseUtils {
      * @param closeables closeables
      */
     public static void closeIOQuietly(final Closeable... closeables) {
-        if (closeables == null) return;
+        if (closeables == null) {
+            return;
+        }
         for (Closeable closeable : closeables) {
             if (closeable != null) {
                 try {
